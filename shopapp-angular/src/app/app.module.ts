becccdf25d203f5432ffadd9cafabbf85ded4cfa
chinhbean09 <chinh0726@gmail.com>
2023-11-23 03:02:48 +0700

    Comple Order and Confirm

--- shopapp-angular/src/app/app.module.ts
+++ shopapp-angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
+import { OrderComponent } from './order/order.component';
+import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 
 @NgModule({
   declarations: [    
     HomeComponent,
-    HeaderComponent,FooterComponent,DetailProductComponent
+    HeaderComponent,FooterComponent,DetailProductComponent,OrderComponent,OrderConfirmComponent
   ],
   imports: [
     BrowserModule
@@ -17,7 +19,10 @@ import { DetailProductComponent } from './detail-product/detail-product.componen
   providers: [],
   bootstrap: [
     // HomeComponent,
-    DetailProductComponent
+    // DetailProductComponent,
+        // OrderComponent,
+    OrderConfirmComponent,
+
   ]
 })
 export class AppModule { }
