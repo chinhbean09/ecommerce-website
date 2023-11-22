45d5f30083c7a0f94c4efb53910d21ef9dde8b85
chinhbean09 <chinh0726@gmail.com>
2023-11-23 02:26:53 +0700

    Done Home Page

--- ShopAngular/src/main.server.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { config } from './app/app.config.server';
-
-const bootstrap = () => {};
-
-export default bootstrap;
--- shopapp-angular/src/app/home/home.component.ts
+++ shopapp-angular/src/app/home/home.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
