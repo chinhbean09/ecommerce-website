65853eea6fb36eb8a26685d02d01a4df7f5f47d1
chinhbean09 <chinh0726@gmail.com>
2023-11-23 18:26:04 +0700

    Done all UI

--- shopapp-angular/src/app/app.module.ts
+++ shopapp-angular/src/app/app.module.ts
@@ -6,11 +6,16 @@ import { FooterComponent } from './footer/footer.component';
 import { DetailProductComponent } from './detail-product/detail-product.component';
 import { OrderComponent } from './order/order.component';
 import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [    
     HomeComponent,
-    HeaderComponent,FooterComponent,DetailProductComponent,OrderComponent,OrderConfirmComponent
+    HeaderComponent,FooterComponent,DetailProductComponent,OrderComponent,
+    OrderConfirmComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
@@ -21,7 +26,9 @@ import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
     // HomeComponent,
     // DetailProductComponent,
         // OrderComponent,
-    OrderConfirmComponent,
+    // OrderConfirmComponent,
+    // LoginComponent,
+    RegisterComponent
 
   ]
 })
