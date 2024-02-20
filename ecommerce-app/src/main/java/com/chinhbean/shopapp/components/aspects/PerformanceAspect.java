7930d40ae9c96135847ee6f69c1ce283bb0f8760
chinhbean09 <chinh0726@gmail.com>
2024-02-20 13:23:12 +0700

    AOP to check time from start to end a controller method

--- ecommerce-app/src/main/java/com/chinhbean/shopapp/components/aspects/PerformanceAspect.java
+++ ecommerce-app/src/main/java/com/chinhbean/shopapp/components/aspects/PerformanceAspect.java
@@ -30,4 +30,16 @@ public class PerformanceAspect {
     public void afterMethodExecution(JoinPoint joinPoint) {
         logger.info("Finished execution of " + this.getMethodName(joinPoint));
     }
+    @Around("controllerMethods()")
+    public Object measureControllerMethodExecutionTime(ProceedingJoinPoint proceedingJoinPoint)
+            throws Throwable {
+        long start = System.nanoTime();
+        //proceedingJoinPoint.getArgs()
+        Object returnValue = proceedingJoinPoint.proceed();
+        long end = System.nanoTime();
+        String methodName = proceedingJoinPoint.getSignature().getName();
+        logger.info("Execution of " + methodName + " took " +
+                TimeUnit.NANOSECONDS.toMillis(end - start) + " ms");
+        return returnValue;
+    }
 }
